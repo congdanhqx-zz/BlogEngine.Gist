4c54e86dc11242142bead0c782843ffcd133bce8
Cong Danh <congdanhqx@live.com>
2013-09-04 23:59:49 +0700

    Initial commit.
    Initial commit for GIST support for BlogEngine.NET

---
+++ GistExtension.cs
@@ -0,0 +1,25 @@
+﻿using BlogEngine.Core;
+using BlogEngine.Core.Web.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BlogEngine.Gist
+{
+    [Extension("This extension enables Github Gist support for BlogEngine.NET.", "1.0", "Cong Danh")]
+    public class GistExtension
+    {
+        public GistExtension()
+        {
+            Post.Serving += Post_Serving;
+        }
+
+        void Post_Serving(object sender, ServingEventArgs e)
+        {
+            e.Body = Regex.Replace(e.Body, "\\[gist\\s[^\\]]*id=(\\d+)[^\\]]*\\]","<script src=\"https://gist.github.com/$1.js\"></script>");
+        }
+    }
+}

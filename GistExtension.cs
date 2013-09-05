9a6c95b667f414662e11d10b8dd5d1734db9988a
Cong Danh <congdanhqx@live.com>
2013-09-05 22:22:34 +0700

    Remove unused using.

--- GistExtension.cs
+++ GistExtension.cs
@@ -1,11 +1,6 @@
 ﻿using BlogEngine.Core;
 using BlogEngine.Core.Web.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace BlogEngine.Gist
 {
